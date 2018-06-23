8bbaf7494f72ee10a9046fe4da52d442d9e0c311
Eric A. Soto <eric@issfl.com>
2018-06-22 22:18:13 -0400

    - support for HTML output including sample layout and style files, a new HtmlOutput class and tweaks to ReportFormat and report in support.

--- report.js
+++ report.js
@@ -2,6 +2,7 @@
 
 // Imports
 const ReportFormat = require('./lib/ReportFormat.js');
+const HtmlOutput = require('./lib/HtmlOutput.js');
 const Promise = require('bluebird');
 
 // Constants
@@ -12,27 +13,45 @@ const conf = require('./conf/conf.json');
  *
  * This script will generate a report for a board and lists configured in ./conf/conf.json.
  *
+ * When outputFormat is set to html or rtf, we expect the conf 'card_otput_format' and 'list_name_format'
+ * to contain markdown which will then converted into html/rtf.
+ *
  */
+
+// Output format (html, rtf, text, or anything else for text)
+let outputFormat = conf.board.output.outputFormat || 'text';
 
 // Decide the lists to print based on the config. Note, the listItem ids must be defined in ./conf/conf.json
 const listsToPrint = conf.board.lists;
+
 // Set an array to hold our promises
 let promiseArray = [];
+let promiseResults = [];
 
 // Loop over the lists to print and setup a promise for each. Note the syntax '() => PromiseFunc()' to prevent the promise from firing now!
 for (let listItem of listsToPrint) {
   promiseArray.push(() => ReportFormat.printListReport(listItem));
 }
 
-// With all the promises in an array, now we want to fire each in sequence.
+// With all the promises in an array, now we want to fire each in sequence and capture results
 Promise.each(promiseArray, (aPromise, index, length) => {
   // Process the one promise (one at a time)
-  return aPromise();
+  return aPromise().then(outputString => {
+    // Save our string from the promise step into the promiseResults array so we can process it later
+    promiseResults[index] = outputString;
+  });
 })
   .then(() => {
-    // All done
+    switch (outputFormat) {
+      case 'html':
+        HtmlOutput.finalOutput(promiseResults, conf);
+        break;
+      default:
+        // Otherwise, just outputFormat raw, but as a string with no delimiters
+        console.log(promiseResults.join(''));
+    }
   })
   .catch(error => {
-    // We have some error, output information about it.
+    // We have some error, outputFormat information about it.
     console.log(`{ "Error": "`, error, `"}`);
   });
